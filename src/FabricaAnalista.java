7625fd65c0a4723be7cfae1ec00e2b1a0184cc53
Davi Manzotti <davimanzotti@gmail.com>
2025-04-08 18:25:08 -0300

    Padrao singleton + diagrama

--- src/FabricaAnalista.java
+++ src/FabricaAnalista.java
@@ -1,6 +1,10 @@
 public class FabricaAnalista implements FabricaCargo{
+    private FabricaAnalista() {};
+    private static FabricaAnalista instance = new FabricaAnalista();
+    public static FabricaAnalista getInstance() {
+        return instance;
+    }
     public Cargo criar(Nivel nivel){
         return new Analista(nivel);
     }
 }
-
--- src/FabricaGerente.java
+++ src/FabricaGerente.java
@@ -1,4 +1,9 @@
 public class FabricaGerente implements FabricaCargo{
+    private FabricaGerente() {};
+    private static FabricaGerente instance = new FabricaGerente();
+    public static FabricaGerente getInstance() {
+        return instance;
+    }
     public  Cargo criar(Nivel nivel){
         return new Gerente(nivel);
     }
